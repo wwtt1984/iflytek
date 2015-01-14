8e1e524724fadcd373a9d169265f23372718ce76
master <wwtt1984@163.com>
2015-01-14 19:48:28 +0800

    2015011419

--- www/iflytek.js
+++ www/iflytek.js
@@ -1,26 +1,4 @@
-﻿var cordova = require('cordova'),
-    exec = require('cordova/exec');
-
-function handlers() {
-  return IflyTek.channels.speakstatus.numHandlers;
-}
-
-var IFlyTek = function() {
-    
-    this.channels = {
-      speakstatus:cordova.addWindowEventHandler("speakstatus"),
-    };
-    for (var key in this.channels) {
-        this.channels[key].onHasSubscribersChange = IFlyTek.onHasSubscribersChange;
-    }
-};
-
-IFlyTek.onHasSubscribersChange = function() {
-	
-      exec(IflyTek._status, null, "IflyTekPlugin", "StartListen", []);
-};
-
-var IflyTek = function () {};
+﻿var IflyTek = function () {};
 /**
  * 设置提示值
  * @param user
@@ -31,22 +9,13 @@ IflyTek.prototype.IflyTekInit = function (appId,callback) {
     return cordova.exec(callback, null,"IflyTekPlugin","Init",[appId]);
 };
 
-/*
 IflyTek.prototype.IflyTekStartListen = function (callback) {
-    return cordova.exec(IflyTek._status, null,"IflyTekPlugin","StartListen",[]);
+    return cordova.exec(callback, null,"IflyTekPlugin","StartListen",[]);
 };
-*/
 
 IflyTek.prototype.IflyTekStopListen = function (user,pwd,callback) {
     return cordova.exec(callback, null,"IflyTekPlugin","StopListen",[user,pwd]);
 };
 
-IflyTek.prototype._status = function (info) {
-
-    if (info) {
-		cordova.fireWindowEvent("speakstatus", info);
-		IflyTek.info = info;
-    }
-};
 
 module.exports = (new IflyTek());
